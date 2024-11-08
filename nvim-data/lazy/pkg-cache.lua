35e556d412d141994dda77756308fbdf4c75b440
Fabio Canavarro <fabiocanavarrotoh@gmail.com>
2024-11-08 19:35:47 +0700

    addnew

--- nvim-data/lazy/pkg-cache.lua
+++ nvim-data/lazy/pkg-cache.lua
@@ -1,7 +1,7 @@
-return {pkgs={{dir="C:/Users/ASUS/AppData/Local/nvim-data/lazy/beacon.nvim",file="lazy.lua",source="lazy",spec=function()
+return {version=12,pkgs={{dir="C:/Users/ASUS/AppData/Local/nvim-data/lazy/beacon.nvim",name="beacon.nvim",spec=function()
 return { 'danilamihailov/beacon.nvim', opts = {} }
 
-end,name="beacon.nvim",},{dir="C:/Users/ASUS/AppData/Local/nvim-data/lazy/noice.nvim",file="lazy.lua",source="lazy",spec=function()
+end,source="lazy",file="lazy.lua",},{dir="C:/Users/ASUS/AppData/Local/nvim-data/lazy/noice.nvim",name="noice.nvim",spec=function()
 return {
   -- nui.nvim can be lazy loaded
   { "MunifTanjim/nui.nvim", lazy = true },
@@ -10,4 +10,4 @@ return {
   },
 }
 
-end,name="noice.nvim",},{dir="C:/Users/ASUS/AppData/Local/nvim-data/lazy/plenary.nvim",file="community",source="lazy",spec={"nvim-lua/plenary.nvim",lazy=true,},name="plenary.nvim",},{dir="C:/Users/ASUS/AppData/Local/nvim-data/lazy/telescope.nvim",file="telescope.nvim-scm-1.rockspec",source="rockspec",spec={"telescope.nvim",build=false,specs={{"nvim-lua/plenary.nvim",lazy=true,},},},name="telescope.nvim",},},version=12,}
+end,source="lazy",file="lazy.lua",},{dir="C:/Users/ASUS/AppData/Local/nvim-data/lazy/plenary.nvim",name="plenary.nvim",spec={"nvim-lua/plenary.nvim",lazy=true,},source="lazy",file="community",},{dir="C:/Users/ASUS/AppData/Local/nvim-data/lazy/telescope.nvim",name="telescope.nvim",spec={"telescope.nvim",build=false,specs={{"nvim-lua/plenary.nvim",lazy=true,},},},source="rockspec",file="telescope.nvim-scm-1.rockspec",},},}
