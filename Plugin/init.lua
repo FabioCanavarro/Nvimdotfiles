827154492bf5ca46039c981916cf96490e536aa6
Fabio Canavarro <128808537+FabioCanavarro@users.noreply.github.com>
2025-06-08 14:58:46 +0700

    Update init.lua

--- Plugin/init.lua
+++ Plugin/init.lua
@@ -354,45 +354,6 @@ opts = function()
 	dependencies = { "nvim-lua/plenary.nvim"},
   lazy=false,
   {
-  "zbirenbaum/copilot.lua",
-  cmd = "Copilot",
-  event = "InsertEnter",
-  config = function()
-    require("copilot").setup({
-  panel = {
-    enabled = true,
-    auto_refresh = false,
-    keymap = {
-      jump_prev = "[[",
-      jump_next = "]]",
-      accept = "<CR>",
-      refresh = "gr",
-      open = "<M-CR>"
-    },
-    layout = {
-      position = "bottom", -- | top | left | right
-      ratio = 0.4
-    },
-  },
-  suggestion = {
-    enabled = true,
-    auto_trigger = true,
-    hide_during_completion = false,
-    debounce = 75,
-    keymap = {
-      accept = "<M-l>",
-      accept_word = false,
-      accept_line = false,
-      next = "<M-]>",
-      prev = "<M-[>",
-      dismiss = "<C-]>",
-    },
-  },
-  copilot_node_command = 'node', -- Node.js version must be > 18.x
-  server_opts_overrides = {},
-})
-  end,
-},{
   'mfussenegger/nvim-dap'
   },
   -- lazy.nvim
